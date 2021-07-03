474078356817d34e4e8f5a94075a716846da2369
Alaaokby <alaaokby@hotmail.com>
2021-07-03 22:19:43 +0200

    function-edit1

--- js/main.js
+++ js/main.js
@@ -14,26 +14,57 @@ function calcAge() {
     var currentYear = d.getFullYear();
     var currentMonth = d.getMonth() + 1;
     var currentDay = d.getDate();
-    if (birthMonth.value <= currentMonth) {
-      var currentAge = `${currentYear - birthYear.value} : ${Math.abs(
-        currentMonth - birthMonth.value
-      )} : ${Math.abs(currentDay - birthDay.value)}`;
-    } else if (
-      birthMonth.value > currentMonth &&
-      birthDay.value <= currentDay
-    ) {
+    if (birthMonth.value < currentMonth) {
+      if (birthDay.value <= currentDay) {
+        var currentAge = `${currentYear - birthYear.value} : ${Math.abs(
+          currentMonth - birthMonth.value
+        )} : ${Math.abs(currentDay - birthDay.value)}`;
+      } else {
+        currentMonth--;
+        var currentAge = `${currentYear - birthYear.value} : ${Math.abs(
+          currentMonth - birthMonth.value
+        )} : ${Math.abs(30 + currentDay - birthDay.value)}`;
+      }
+    } else if (birthMonth.value > currentMonth) {
       currentYear--;
-      var currentAge = `${currentYear - birthYear.value} : ${
-        12 - Math.abs(currentMonth - birthMonth.value)
-      } : ${Math.abs(currentDay - birthDay.value)}`;
-    } else if (birthMonth.value > currentMonth && birthDay.value > currentDay) {
-      currentYear--;
-      currentMonth--;
-      var currentAge = `${currentYear - birthYear.value} : ${
-        12 - Math.abs(currentMonth - birthMonth.value)
-      } : ${31 - Math.abs(currentDay - birthDay.value)}`;
+      if (birthDay.value <= currentDay) {
+        var currentAge = `${currentYear - birthYear.value} : ${
+          12 - Math.abs(currentMonth - birthMonth.value)
+        } : ${Math.abs(currentDay - birthDay.value)}`;
+      } else {
+        currentMonth--;
+        var currentAge = `${currentYear - birthYear.value} : ${
+          12 - Math.abs(currentMonth - birthMonth.value)
+        } : ${Math.abs(30 + currentDay - birthDay.value)}`;
+      }
+    } else if (birthMonth.value == currentMonth) {
+      if (birthDay.value > currentDay) {
+        currentYear--;
+        var currentAge = `${currentYear - birthYear.value} : 
+          11 
+         : ${Math.abs(30 + currentDay - birthDay.value)}`;
+      } else {
+        var currentAge = `${currentYear - birthYear.value} : ${Math.abs(
+          currentMonth - birthMonth.value
+        )} : ${Math.abs(currentDay - birthDay.value)}`;
+      }
     }
+
+    // else if (birthMonth.value = currentMonth) {
+    //   if(birthDay.value = currentDay){
+
+    //   }
+    // }
+    // } else if (birthMonth.value > currentMonth && birthDay.value > currentDay) {
+    //   currentYear--;
+    //   currentMonth--;
+    //   var currentAge = `${currentYear - birthYear.value} : ${
+    //     12 - Math.abs(currentMonth - birthMonth.value)
+    //   } : ${31 - Math.abs(currentDay - birthDay.value)}`;
+    // }
     console.log(currentAge);
+    console.log(currentDay);
+
     age.innerHTML = `Your age is ${currentAge}`;
   }
 }
